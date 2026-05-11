cb784b3f0119440a94b705a4a914cc15eb8fe07a
Abdul-Barr Mohammed <barrmohammed.tpa@gmail.com>
2026-05-10 22:14:08 -0400

    Completed first attempt of CRUD operations for user

--- src/main/java/com/blink/blink_backend/controllers/UserController.java
+++ src/main/java/com/blink/blink_backend/controllers/UserController.java
@@ -1,15 +1,18 @@
 package com.blink.blink_backend.controllers;
 
 
+import com.blink.blink_backend.dto.BoardDto;
 import com.blink.blink_backend.dto.UserDto;
+import com.blink.blink_backend.entities.Board;
+import com.blink.blink_backend.entities.User;
 import com.blink.blink_backend.mappers.UserMapper;
 import com.blink.blink_backend.services.UserService;
 import com.blink.blink_backend.services.impl.UserServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/user")
@@ -30,5 +33,41 @@ public class UserController {
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public UserDto createUser(@RequestBody UserDto userDto) {
+
+        User createdUser = userService.createUser(
+                userMapper.fromDto(userDto)
+        );
+
+        return userMapper.toDto(createdUser);
+
+    }
+
+    @GetMapping(path = "/{user_id}")
+    public Optional<UserDto> getUser(@PathVariable("user_id") UUID userId) {
+        return userService.getUser(userId).map(userMapper::toDto);
+    }
+
+    @PutMapping(path = "/{user_id}")
+    public UserDto updatedUser(
+            @PathVariable("user_id") UUID userId,
+            @RequestBody UserDto userDto
+    )
+    {
+        User updatedUser = userService.updateUser(
+                userId,
+                userMapper.fromDto(userDto)
+
+        );
+
+        return userMapper.toDto(updatedUser);
+    }
+
+    @DeleteMapping(path = "/{user_id}")
+    public void deleteUser(@PathVariable("user_id") UUID userId) {
+        userService.deleteUser(userId);
     }
 }
